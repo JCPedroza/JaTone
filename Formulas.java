dea1b77d745c9e729e0b6d96c209c85f3307fd50
JCPedroza <nemesis4go10@hotmail.com>
2013-07-22 14:31:21 -0600

    added pentatonic and hexatonic formulas

---
+++ Formulas.java
@@ -0,0 +1,16 @@
+public class Formulas{
+
+    // Formulas for pentatonic scales
+    int[] pentmin = {3, 2, 2, 3, 2};
+    int[] pentmaj = {2, 2, 3, 2, 3};
+
+    // Formulas for hexatonic scales
+    int[] minblues   = {3, 2, 1, 1, 3, 2};
+    int[] majblues   = {2, 1, 1, 3, 2, 3};
+    int[] prometheus = {2, 2, 2, 3, 1, 2};
+    int[] istrian    = {1, 2, 1, 2, 1, 5};
+    int[] wholetone  = {2, 2, 2, 2, 2, 2};
+    int[] augmented  = {3, 1, 3, 1, 3, 1};
+    int[] tritone    = {1, 3, 2, 1, 3, 2};
+    int[] twosemitri = {1, 1, 4, 1, 1, 4};
+}
