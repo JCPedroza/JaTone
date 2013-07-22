6a42468381f129d284a6d3a808f3b218381fbda6
JCPedroza <nemesis4go10@hotmail.com>
2013-07-22 14:35:03 -0600

    tests for Formulas ans Note

--- main.java
+++ main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
 public class main{
 
     public static void main(String args[]){
-        System.out.println("test");
+        Sound    aSound   = new Sound(440.03, 60, 1, 1, 127, 1);
+        Note     aNote    = new Note("A", 440.00, 60, 1, 1, 127, 1);
+        Formulas aFormula = new Formulas();
+
+        System.out.println(aSound.getFrequency());
+        System.out.println(aNote.getName());
+        System.out.println(Arrays.toString(aFormula.pentmin));
     }
 }
