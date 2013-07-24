d9acd99925536bf6a4176d38888ee6af42d7ae4f
JCPedroza <nemesis4go10@hotmail.com>
2013-07-24 06:11:06 -0600

    new tests

--- main.java
+++ main.java
@@ -1,14 +1,31 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main{
 
     public static void main(String args[]){
         Sound    aSound   = new Sound(440.03, 60, 1, 1, 127, 1);
-        Note     aNote    = new Note("A", 440.00, 60, 1, 1, 127, 1);
-        Formulas aFormula = new Formulas();
 
-        System.out.println(aSound.getFrequency());
-        System.out.println(aNote.getName());
-        System.out.println(Arrays.toString(aFormula.pentmin));
+        Note            aNoteA      = new Note("A", 440.00, 60, 1, 1, 127, 1);
+        Note            aNoteC      = new Note("C", 500.12, 60, 1, 1, 127, 1);
+        Note            aNoteE      = new Note("E", 510.33, 60, 1, 1, 127, 1);
+        Note[]          aNoteArray  = {aNoteA, aNoteC, aNoteE};
+        List<Note>      aNoteAL     = new ArrayList<Note>();
+        aNoteAL.add(aNoteA); aNoteAL.add(aNoteC); aNoteAL.add(aNoteE);
+
+        Formulas        aFormula    = new Formulas();
+ 
+        NoteGroup       aNoteGroup1 = new NoteGroup(aNoteArray);
+        NoteGroup       aNoteGroup2 = new NoteGroup(aNoteAL);
+        Chord           aChord1     = new Chord(aNoteArray, "name1");
+        Chord           aChord2     = new Chord(aNoteAL, "name2");
+
+        System.out.println(aChord1.getSize());
+        System.out.println(aChord1.getName());
+        System.out.println(aChord2.getName());
+        for (Note note : aChord2.getNotes()){
+            System.out.println(note.getName());
+        }
     }
 }
