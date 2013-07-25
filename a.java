61d71a3bff7dbdeff25a6059a104d6e9ceb4d75b
JCPedroza <nemesis4go10@hotmail.com>
2013-07-24 18:41:34 -0600

    changed name from name.java to a.java

--- a.java
+++ a.java
@@ -1,23 +1,16 @@
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class main{
+public class a{
 
     public static void main(String args[]){
-        Sound    aSound   = new Sound(440.03, 60, 1, 1, 127, 1);
-
         Note            aNoteA      = new Note("A", 440.00, 60, 1, 1, 127, 1);
         Note            aNoteC      = new Note("C", 500.12, 60, 1, 1, 127, 1);
         Note            aNoteE      = new Note("E", 510.33, 60, 1, 1, 127, 1);
         Note[]          aNoteArray  = {aNoteA, aNoteC, aNoteE};
         List<Note>      aNoteAL     = new ArrayList<Note>();
         aNoteAL.add(aNoteA); aNoteAL.add(aNoteC); aNoteAL.add(aNoteE);
-
-        Formulas        aFormula    = new Formulas();
  
-        NoteGroup       aNoteGroup1 = new NoteGroup(aNoteArray);
-        NoteGroup       aNoteGroup2 = new NoteGroup(aNoteAL);
         Chord           aChord1     = new Chord(aNoteArray, "name1");
         Chord           aChord2     = new Chord(aNoteAL, "name2");
 
