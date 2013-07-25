e639b205e6be571325ed2590f7b4e5908e6661e7
JCPedroza <nemesis4go10@hotmail.com>
2013-07-25 10:56:23 -0600

    test cases for some classes and stepCount in Process.java

--- Test.java
+++ Test.java
@@ -1,10 +1,34 @@
-import org.junit.*;
 import static org.junit.Assert.*;
+
+import java.util.*;
 
 public class Test{
 	
-	public void runTests(){
+	// Instance variables for use with testing
+	Note         aNoteA     = new Note("A", 440, 60, 1, 1, 127, 1);
+	Note         aNoteC     = new Note("C", 450, 61, 2, 4, 125, 6);
+	Note         aNoteE     = new Note("E", 460, 62, 3, 5, 126, 7);
+	Note         aNoteG     = new Note("G", 470, 66, 6, 6, 6, 6);
+	Note[]       aNoteArray = {aNoteA, aNoteC, aNoteE};
+	List<Note>   aNoteList  = Arrays.asList(aNoteA, aNoteC, aNoteE);
+	List<String> aPool      = Arrays.asList("A", "B", "C", "D");
+	Chord        aChord1    = new Chord(aNoteArray, "Am");
+	Scale        aScale1    = new Scale(aNoteList, "A aeolian");
+	
+	// Process instance
+	Process    aProcess   = new Process();
+
+	public void runTests(){		
+		// Class Tests
+		assertEquals(aNoteA.getName(), "A");
+		assertEquals(aScale1.getName(), "A aeolian");
+		assertEquals(aChord1.getName(), "Am");
 		
-		assertEquals(":D", 2, 2);
+		// Process Tests
+		
+		//stepCount()
+		assertEquals(aProcess.stepCount(aNoteA, aNoteC), 3);
+		assertEquals(aProcess.stepCount(aNoteG, aNoteA), 2);
+		assertEquals(aProcess.stepCount(aNoteA, aNoteC, aPool), 2);
 	}
 }
