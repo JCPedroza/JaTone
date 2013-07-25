8d0781de005c120d08b932dff75f4f5c2ae6c919
JCPedroza <nemesis4go10@hotmail.com>
2013-07-25 16:12:44 -0600

    implemented overloaded constructor with default parameters for only String input case

--- Note.java
+++ Note.java
@@ -3,10 +3,13 @@ public class Note extends Sound{
     // Instance variables
     private String name;
     
-    // Constructor
+    // Constructors
     public Note(String startName, double freq, int vel, int att, int dec, int sus, int rel){
         super(freq, vel, att, dec, sus, rel);
         name = startName;
+    }
+    public Note(String startName){
+    	this(startName, -1, 63, 1, 1, 127, 1);  	
     }
 
     // Setters & Getters
