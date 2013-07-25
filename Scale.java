c4ac2c56f897d2e9c60a0eb796b4b283701d004f
JCPedroza <nemesis4go10@hotmail.com>
2013-07-25 07:34:23 -0600

    Scale.java first commit

---
+++ Scale.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Scale extends NoteGroup{
+	// Instance variables
+    private String name;
+
+    // Constructors
+    // Accepts array or List as input, creates an ArralyList with it.
+    public Scale(List<Note> newNotes, String newName){
+        super(newNotes);
+        name = newName;
+    }
+    public Scale(Note[] newNotes, String newName){
+        this(new ArrayList<Note>(Arrays.asList(newNotes)), newName);
+    }
+
+    //Setters & Getters
+    public void setName(String newName){
+        name = newName;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
