d1b698dced16d9fc5c33fe765ef7c989d4873774
JCPedroza <nemesis4go10@hotmail.com>
2013-07-26 12:12:17 -0600

    more formulas

--- Formulas.java
+++ Formulas.java
@@ -1,16 +1,21 @@
 public class Formulas{
 
     // Formulas for pentatonic scales
-    int[] pentmin = {3, 2, 2, 3, 2};  // minor pentatonic
-    int[] pentmaj = {2, 2, 3, 2, 3};  // major pentatonic
-
+    public int[] pentmin = {3, 2, 2, 3, 2};  // minor pentatonic
+    public int[] pentmaj = {2, 2, 3, 2, 3};  // major pentatonic
+    
     // Formulas for hexatonic scales
-    int[] minblues   = {3, 2, 1, 1, 3, 2};  // minor blues 
-    int[] majblues   = {2, 1, 1, 3, 2, 3};  // major blues
-    int[] prometheus = {2, 2, 2, 3, 1, 2};  // prometheus - mystic
-    int[] istrian    = {1, 2, 1, 2, 1, 5};  // istrian
-    int[] wholetone  = {2, 2, 2, 2, 2, 2};  // whole tone
-    int[] augmented  = {3, 1, 3, 1, 3, 1};  // augmented - symmetrical augmented
-    int[] tritone    = {1, 3, 2, 1, 3, 2};  // tritone 
-    int[] twosemitri = {1, 1, 4, 1, 1, 4};  // two semitone tritone 
+    public int[] minblues   = {3, 2, 1, 1, 3, 2};  // minor blues 
+    public int[] majblues   = {2, 1, 1, 3, 2, 3};  // major blues
+    public int[] prometheus = {2, 2, 2, 3, 1, 2};  // prometheus - mystic
+    public int[] istrian    = {1, 2, 1, 2, 1, 5};  // istrian
+    public int[] wholetone  = {2, 2, 2, 2, 2, 2};  // whole tone
+    public int[] augmented  = {3, 1, 3, 1, 3, 1};  // augmented - symmetrical augmented
+    public int[] tritone    = {1, 3, 2, 1, 3, 2};  // tritone 
+    public int[] twosemitri = {1, 1, 4, 1, 1, 4};  // two semitone tritone 
+    
+    // Major derived formulas
+    public int[] aeolian    = {2, 1, 2, 2, 1, 2, 2};  // aeolian - minor
+    public int[] ionian     = {2, 2, 1, 2, 2, 2, 1};  // ionian - major
+    public int[] mixolydian = {2, 2, 1, 2, 2, 1, 2};  // mixolydian
 }
