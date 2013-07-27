d28dc241f85f65001702d7fee06aeccb372e81e8
JCPedroza <nemesis4go10@hotmail.com>
2013-07-27 11:09:53 -0600

    1: implemented overloads for stepCount for String input cases and 2: implemented overloads for scalize for many different cases

--- Process.java
+++ Process.java
@@ -6,8 +6,12 @@ public class Process{
 	private List<String> et12Pool   = Arrays.asList("Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G");
 	private int          et12Length = 12;
 	
-	// !!! Throw error when note is not found in pool
+	// ---------------------------------------------------------------------------------------------
 	// stepCount: returns the distance between two notes using an input pool of type List
+	// Note, Note, optional: List or String[] -> int or String, String, optional: List or String[]-> int
+	// !!! Throw error when note is not found in pool
+	// ---------------------------------------------------------------------------------------------
+	// stepCount: Note, Note, List -> int 
 	public int stepCount(Note note1, Note note2, List<String> pool){
 	    int result = pool.indexOf(note2.getName()) - pool.indexOf(note1.getName());
 		if (result > 0){
@@ -17,13 +21,35 @@ public class Process{
 			return result + et12Length;
 		}
 	}
-	// stepCount: overloaded: no input pool, uses default pool
+	// stepCount: Note, Note -> int
 	public int stepCount(Note note1, Note note2){
 		return stepCount(note1, note2, et12Pool);
 	}
+	// stepCount: Note, Note, String[] -> int
+	public int stepCount(Note note1, Note note2, String[] pool){
+		return stepCount(note1, note2, new ArrayList<String>(Arrays.asList(pool)));
+	}
+	// stepCount: String, String -> int
+	public int stepCount(String note1, String note2){
+		return stepCount(new Note(note1), new Note(note2), et12Pool);
+	}
+	// stepCount: String, String, List -> int
+	public int stepCount(String note1, String note2, List<String> pool){
+		return stepCount(new Note(note1), new Note(note2), pool);
+	}
+	// stepCount: String, String, String[] -> int
+	public int stepCount(String note1, String note2, String[] pool){
+		return stepCount(new Note(note1), new Note(note2), new ArrayList<String>(Arrays.asList(pool)));
+	}
+
 	
-	// scalize: returns a Scale object derived from the formula applied to the pool with note as root
-	// !!! implement overloads for pool inputs, both array and list 
+	// ---------------------------------------------------------------------------------------------
+	// scalize: returns a Scale object derived from the formula applied to the pool with note as root,
+	// default pool is et12Pool. 
+	// Note or String, int[], optional: List or String[] -> Scale
+	// !!! throw errors: note was not found in pool
+	// ---------------------------------------------------------------------------------------------
+	// scalize: Note, int[], List -> Scale
 	public Scale scalize(Note note, int[] formula, List<String> pool){
 	    List<Note> noteList = new ArrayList<Note>();
 	    int currentIndex = pool.indexOf(note.getName());
@@ -33,12 +59,25 @@ public class Process{
 	    }
 	    return new Scale(noteList);  
 	}
-	// scalize: overloaded: array as input pull
+	// scalize: Note, int[], String[] -> Scale
 	public Scale scalize(Note note, int[] formula, String[] pool){
 	    return scalize(note, formula, new ArrayList<String>(Arrays.asList(pool)));
 	}
-	// scalize: overloaded: no input pool, uses default pool as input
+	// scalize: overloaded: no input pool, uses default pool 
 	public Scale scalize(Note note, int[] formula){
 	    return scalize(note, formula, et12Pool); 
 	}
+	// scalize: overloaded: note as string
+	public Scale scalize(String note, int[] formula, List<String> pool){
+		return scalize(new Note(note), formula, pool);
+	}
+	// scalize: overloaded: note as string, pool as array 
+    public Scale scalize(String note, int[] formula, String[] pool){
+		return scalize(new Note(note), formula, new ArrayList<String>(Arrays.asList(pool)));
+	}
+	// scalize: overloaded: note as string, no input pool, uses default pool 
+	public Scale scalize(String note, int[] formula){
+		return scalize(new Note(note), formula, et12Pool);
+	}
+	
 }
