10e2ec54cafcc7b94b774770b58bbc5c8598d30b
JCPedroza <nemesis4go10@hotmail.com>
2013-08-21 09:41:29 -0600

    javadoc comments for instance variables

--- Sound.java
+++ Sound.java
@@ -3,7 +3,7 @@
 /**
 * Superclass for everything that produces a sound. The instance variables include only the most basic characteristics of a sound:
 * the frequency, the velocity, and the envelope (attack, decay sustain, release). Other classes that extend Sound implement other
-* characteristics, like name. 
+* characteristics, like name.
 */
 public class Sound{
     
@@ -11,11 +11,33 @@ public class Sound{
     //                     Instance Variables
     //===================================================================
 	
+	/**
+	* The frequency of the sound in no particular unit. A frequency of -1 means that the value was not set, and it is the default value. 
+	* Hertz are recommended as unit for compatibility with other devices, programs, and dynamics. 
+	*/
     private double frequency;
+    /**
+    * The velocity of the sound in no particular unit. MIDI range (0 to 127) is recommended for compatibility with MIDI dynamics. 
+    */
     private int    velocity;
+    /**
+    * The time it takes the sound to reach its highest amplitude. It has no particular unit, and MIDI range (0 to 127) is recommended 
+    * for compatibility with MIDI dynamics.
+    */
     private int    attack;
+    /**
+    * The time it takes the sound to reach the sustain level after the highest amplitude was reached. It has no particular unit, 
+    * and MIDI range (0 to 127) is recommended for compatibility with MIDI dynamics.
+    */
     private int    decay;
+    /**
+    * The amplitude of the sound will keep while true. It has no particular unit, and MIDI range (0 to 127) is recommended for 
+    * compatibility with MIDI dynamics.
+    */
     private int    sustain;
+    /**
+    * The time it takes the sound to reach zero amplitude from its current amplitude, it's triggered by soundOn = false.
+    */
     private int    release;
     
     //===================================================================
@@ -42,34 +64,6 @@ public class Sound{
     public String toString(){
     	return  "frequency: " + frequency + " velocity: " + velocity + " attack: "  + attack +
     			" decay: "    + decay     + " sustain: "  + sustain  + " release: " + release;
-    }
-    
-    //===================================================================
-    //                          Getters
-    //===================================================================
-    
-    public double getFrequency(){
-        return frequency;
-    }
-    
-    public int getVelocity(){
-        return velocity;
-    }
-    
-    public int getAttack(){
-        return attack;
-    }
-    
-    public int getDecay(){
-        return decay;
-    }
-    
-    public int getSustain(){
-        return sustain;
-    }
-    
-    public int getRelease(){
-        return release;
     }
     
     //===================================================================
@@ -100,4 +94,31 @@ public class Sound{
         release = rel;
     }
 
+    //===================================================================
+    //                          Getters
+    //===================================================================
+    
+    public double getFrequency(){
+        return frequency;
+    }
+    
+    public int getVelocity(){
+        return velocity;
+    }
+    
+    public int getAttack(){
+        return attack;
+    }
+    
+    public int getDecay(){
+        return decay;
+    }
+    
+    public int getSustain(){
+        return sustain;
+    }
+    
+    public int getRelease(){
+        return release;
+    }
 }
