a287f162c5e6a38b440e5b327fb317676f1895db
JCPedroza <nemesis4go10@hotmail.com>
2013-08-22 11:22:45 -0600

    changed instance names to upper case underscore convention and some important comments/notes

--- Formulas.java
+++ Formulas.java
@@ -1,5 +1,9 @@
-// !!! getters
-
+// consider not using a Formulas class for your constants and add them to the class(es) that might need them instead, as suggested
+// in: http://stackoverflow.com/questions/66066/what-is-the-best-way-to-implement-constants-in-java
+// consider using other type other than primitive arrays, those can't be made not-mutable and constants need to be not-mutable
+// as suggested: http://stackoverflow.com/questions/3700971/immutable-array-in-java
+// you might also want to make these public if they are not implemented inside the class that needs them, as
+// sugested here: http://stackoverflow.com/questions/10047802/public-static-final-or-private-static-final-with-getter
 public class Formulas{
     
 	//===================================================================
@@ -7,41 +11,41 @@ public class Formulas{
     //===================================================================
 	
     // Formulas for pentatonic scales
-    private int[] minorPentatonic = {3, 2, 2, 3, 2};  // minor pentatonic
-    private int[] majorPentatonic = {2, 2, 3, 2, 3};  // major pentatonic
+    private int[] MINOR_PENTATONIC = {3, 2, 2, 3, 2};  // minor pentatonic
+    private int[] MAJOR_PENTATONIC = {2, 2, 3, 2, 3};  // major pentatonic
     
     // Formulas for hexatonic scales
-    private int[] minblues           = {3, 2, 1, 1, 3, 2};  // minor blues 
-    private int[] majblues           = {2, 1, 1, 3, 2, 3};  // major blues
-    private int[] prometheus         = {2, 2, 2, 3, 1, 2};  // prometheus - mystic
-    private int[] istrian            = {1, 2, 1, 2, 1, 5};  // istrian
-    private int[] wholeTone          = {2, 2, 2, 2, 2, 2};  // whole tone
-    private int[] augmented          = {3, 1, 3, 1, 3, 1};  // augmented - symmetrical augmented
-    private int[] tritone            = {1, 3, 2, 1, 3, 2};  // tritone 
-    private int[] twoSemitoneTritone = {1, 1, 4, 1, 1, 4};  // two semitone tritone 
+    private int[] MINOR_BLUES          = {3, 2, 1, 1, 3, 2};  // minor blues 
+    private int[] MAJOR_BLUES          = {2, 1, 1, 3, 2, 3};  // major blues
+    private int[] PROMETHEUS           = {2, 2, 2, 3, 1, 2};  // prometheus - mystic
+    private int[] ISTRIAN              = {1, 2, 1, 2, 1, 5};  // istrian
+    private int[] WHOLE_TONE           = {2, 2, 2, 2, 2, 2};  // whole tone
+    private int[] AUGMENTED            = {3, 1, 3, 1, 3, 1};  // augmented - symmetrical augmented
+    private int[] TRITONE              = {1, 3, 2, 1, 3, 2};  // tritone 
+    private int[] TWO_SEMITONE_TRITONE = {1, 1, 4, 1, 1, 4};  // two semitone tritone 
     
     // Major derived formulas
-    private int[] lydian     = {2, 2, 2, 1, 2, 2, 1};  // lydian
-    private int[] ionian     = {2, 2, 1, 2, 2, 2, 1};  // ionian - major
-    private int[] mixolydian = {2, 2, 1, 2, 2, 1, 2};  // mixolydian
-    private int[] dorian     = {2, 1, 2, 2, 2, 1, 2};  // dorian
-    private int[] aeolian    = {2, 1, 2, 2, 1, 2, 2};  // aeolian - minor
-    private int[] phrygian   = {1, 2, 2, 2, 1, 2, 2};  // phrygian
-    private int[] locrian    = {1, 2, 2, 1, 2, 2, 2};  // locrian
+    private int[] LYDIAN     = {2, 2, 2, 1, 2, 2, 1};  // lydian
+    private int[] IONIAN     = {2, 2, 1, 2, 2, 2, 1};  // ionian - major
+    private int[] MIXOLYDIAN = {2, 2, 1, 2, 2, 1, 2};  // mixolydian
+    private int[] DORIAN     = {2, 1, 2, 2, 2, 1, 2};  // dorian
+    private int[] AEOLIAN    = {2, 1, 2, 2, 1, 2, 2};  // aeolian - minor
+    private int[] PHRYGIAN   = {1, 2, 2, 2, 1, 2, 2};  // phrygian
+    private int[] LOCRIAN    = {1, 2, 2, 1, 2, 2, 2};  // locrian
     
     // Melodic minor derived formulas
-    private int[] melodicMinor     = {2, 1, 2, 2, 2, 2, 1}; // melodic minor
-    private int[] dorianFlat9      = {1, 2, 2, 2, 2, 1, 2}; // dorian flat 9
-    private int[] lydianAugmented  = {2, 2, 2, 2, 1, 2, 1}; // lydian augmented
-    private int[] lydianFlat7      = {2, 2, 2, 1, 2, 1, 2}; // lydian flat 7
-    private int[] mixolydianFlat13 = {2, 2, 1, 2, 1, 2, 2}; // mixolydian flat 13
-    private int[] semiLocrian      = {2, 1, 2, 1, 2, 2, 2}; // semilocrian
-    private int[] superLocrian     = {1, 2, 1, 2, 2, 2, 2}; // superlocrian
+    private int[] MELODIC_MINOR     = {2, 1, 2, 2, 2, 2, 1}; // melodic minor
+    private int[] DORIAN_FLAT9      = {1, 2, 2, 2, 2, 1, 2}; // dorian flat 9
+    private int[] LYDIAN_AUGMENTED  = {2, 2, 2, 2, 1, 2, 1}; // lydian augmented
+    private int[] LYDIAN_FLAT7      = {2, 2, 2, 1, 2, 1, 2}; // lydian flat 7
+    private int[] MIXOLYDIAN_FLAT13 = {2, 2, 1, 2, 1, 2, 2}; // mixolydian flat 13
+    private int[] SEMI_LOCRIAN      = {2, 1, 2, 1, 2, 2, 2}; // semilocrian
+    private int[] SUPER_LOCRIAN     = {1, 2, 1, 2, 2, 2, 2}; // superlocrian
     
     // Harmonic minor derived formulas
-    private int[] harmonicMinor   = {2, 1, 2, 2, 1, 3, 1}; // harmonic minor
-    private int[] locrianSharp7   = {1, 2, 2, 1, 3, 1, 2}; // locrian sharp 7
-    private int[] ionianAugmented = {2, 2, 1, 3, 1, 2, 1}; // ionian augmented
+    private int[] HARMONIC_MINOR   = {2, 1, 2, 2, 1, 3, 1}; // harmonic minor
+    private int[] LOCRIAN_SHARP7   = {1, 2, 2, 1, 3, 1, 2}; // locrian sharp 7
+    private int[] IONIAN_AUGMENTED = {2, 2, 1, 3, 1, 2, 1}; // ionian augmented
     
     //===================================================================
     //                            Getters 
@@ -49,93 +53,93 @@ public class Formulas{
     
     // Getters for pentatonic scale formulas
     public int[] getMinorPentatonic(){
-    	return minorPentatonic;
+    	return MINOR_PENTATONIC;
     }
     public int[] getMajorPentatonic(){
-    	return majorPentatonic;
+    	return MAJOR_PENTATONIC;
     }
     
     // Getters for hexatonic scale formulas
     public int[] getMinBlues(){
-    	return minblues;
+    	return MINOR_BLUES;
     }
     public int[] getMajBlues(){
-    	return majblues;
+    	return MAJOR_BLUES;
     }
     public int[] getTritone(){
-    	return tritone;
+    	return TRITONE;
     }
     public int[] getAugmented(){
-    	return augmented;
+    	return AUGMENTED;
     }
     public int[] getIstrian(){
-    	return istrian;
+    	return ISTRIAN;
     }
     public int[] getWholeTone(){
-    	return wholeTone;
+    	return WHOLE_TONE;
     }
     public int[] getPrometheus(){
-    	return prometheus;
+    	return PROMETHEUS;
     }
     public int[] getTwoSemitoneTritone(){
-    	return twoSemitoneTritone;
+    	return TWO_SEMITONE_TRITONE;
     }
     
     // Getters for melodic minor derived formulas
     public int[] getMelodicMinor(){
-    	return melodicMinor;
+    	return MELODIC_MINOR;
     }
     public int[] getDorianFlat9(){
-    	return dorianFlat9;
+    	return DORIAN_FLAT9;
     }
     public int[] getLydianAugmented(){
-    	return lydianAugmented;
+    	return LYDIAN_AUGMENTED;
     }
     public int[] getLydianFlat7(){
-    	return lydianFlat7;
+    	return LYDIAN_FLAT7;
     }
     public int[] getMixolydianFlat13(){
-    	return mixolydianFlat13;
+    	return MIXOLYDIAN_FLAT13;
     }
     public int[] getSemiLocrian(){
-    	return semiLocrian;
+    	return SEMI_LOCRIAN;
     }
     public int[] getSuperLocrian(){
-    	return superLocrian;
+    	return SUPER_LOCRIAN;
     }
     
     // Getters for major derived formulas
     public int[] getLydian(){
-    	return lydian;
+    	return LYDIAN;
     }
     public int[] getIonian(){
-    	return ionian;
+    	return IONIAN;
     }
     public int[] getMixolydian(){
-    	return mixolydian;
+    	return MIXOLYDIAN;
     }
     public int[] getDorian(){
-    	return dorian;
+    	return DORIAN;
     }
     public int[] getAeolian(){
-    	return aeolian;
+    	return AEOLIAN;
     }
     public int[] getPhrygian(){
-    	return phrygian;
+    	return PHRYGIAN;
     }
     public int[] getLocrian(){
-    	return locrian;
+    	return LOCRIAN;
     }
     
     // Getters for harmonic minor derived formulas
     public int[] getHarmonicMinor(){
-    	return harmonicMinor;
+    	return HARMONIC_MINOR;
     }
     public int[] getLocrianSharp7(){
-    	return locrianSharp7;
+    	return LOCRIAN_SHARP7;
     }
     public int[] getIonianAugmented(){
-    	return ionianAugmented;
+    	return IONIAN_AUGMENTED;
     }
    
 }
